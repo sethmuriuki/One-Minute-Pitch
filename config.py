16077bdc3425748830094d5360ac3a2a5ce71bcd
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-30 16:49:22 +0300

    add configuration to config file and folder structre

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+     '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://seth:seth@localhost/pitch'
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
