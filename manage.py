caec6f0e92a2b339cc8ab120f2c533ccd9be9058
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-30 22:10:52 +0300

    configure the app init file

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-from app import create_app,db
-from flask_script import Manager,Server
+from app import create_app, db
+from flask_script import Manager, Server
 from app.models import Category,User,Peptalk,Comments
 from  flask_migrate import Migrate, MigrateCommand
 from flask_login import LoginManager
