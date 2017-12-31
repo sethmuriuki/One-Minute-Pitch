5d8a3c2ba0aacc6d847a345fc3bd0f1a685e2c97
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-31 19:00:06 +0300

    create root page function

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,16 @@ from ..models import Category, User,Peptalk, Comments
 from .. import db
 from flask_login import login_required, current_user
 from .forms import PeptalkForm,CommentForm
+
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    categories = Category.get_categories()
+    title = 'Home - Welcome to One Minute Pitch'
+    return render_template('index.html', title = title, categories = categories)
+
