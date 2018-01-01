5b0fdbbbe7fa765dc2c5bfbec3460fab71db6808
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-01 16:36:46 +0300

    create dynamic routing for pitches

--- app/main/views.py
+++ app/main/views.py
@@ -17,3 +17,25 @@ def index():
     title = 'Home - Welcome to One Minute Pitch'
     return render_template('index.html', title = title, categories = categories)
 
+# Dynamic routing for pitches
+@main.route('/category/pitch/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_pitch(id):
+    '''
+    Function to check Pitches form
+    '''
+    form = PeptalkForm()
+    category = Category.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        # user = current_user._get_current_object()
+        new_pitch = Peptalk(content=content,user_id=current_user.id,category_id=category.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id = category.id))
+
+    title = 'New pitch'
+    return render_template('new_pitches.html', title = title, pitch_form = form)
