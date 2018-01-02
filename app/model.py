ae80e8aacd8a5d6cb9b2a3e021ec5ef110334e50
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-02 16:22:24 +0300

    add class for users in model

--- app/model.py
+++ app/model.py
@@ -66,3 +66,35 @@ class Talks(db.Model):
         pitches = Talks.query.order_by(Talks.date_posted.desc()).filter_by(category_id=id).all()
         return pitches
 
+#users
+class User(UserMixin,db.Model):
+    '''
+    user class to create new users
+    '''
+    __tablename__ = 'users'
+
+    #addidng columns
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship("Talks", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+
+    #password security
+    @property
+    def password(self):
+        raise AttributeError('you can not read the password Attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+    
